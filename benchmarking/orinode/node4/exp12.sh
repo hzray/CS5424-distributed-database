72e313e5744d8acc53185e5fddaf6beb56d7ec63
RazzorLeaf <rogerforrio@gmail.com>
2021-11-01 18:21:41 +0800

    Update: Experiment Shell

---
+++ benchmarking/orinode/node4/exp12.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+# Node 4 - Exp 1~2
+
+WORKLOAD=$1
+SCRIPT_PATH="../src/main.py"
+
+if [ "$WORKLOAD" == "A" ]
+then
+	XACT_FILE_DIR="/temp/project_files/project_files_cassandra/xact_files_A/"
+else
+	XACT_FILE_DIR="/temp/project_files/project_files_cassandra/xact_files_B/"
+fi
+
+# echo ${XACT_FILE_DIR} 
+for cid in {4..40..5}
+do
+    echo "Excuting Client $cid.txt for Workload $WORKLOAD"
+    python3 $SCRIPT_PATH $WORKLOAD $cid < $XACT_FILE_DIR$cid.txt
+done
